75e6d4bed8b44f45a3a698594ca5a0b4dfcb2d11
unknown <gdutt2304@gmail.com>
2025-12-24 15:27:14 +0530

    File System

---
+++ fileSystem/app1.js
@@ -0,0 +1,18 @@
+// file Sytem  - fs
+// const fs = require("fs");
+// fs.readFile("file1.txt", "utf-8", (err, data) => {
+//   if (err) {
+//     console.error("File read error : ", err);
+//     return;
+//   }
+//   console.log("File Read :  ", data);
+// });
+
+// writeFile('file Type ','DATA', callback)
+// fs.writeFile("code1.html", "i am html file", (err) => {
+//   if (err) {
+//     console.error("Error writing file", err);
+//     return;
+//   }
+//   console.log("file Written Successfully!");
+// });
