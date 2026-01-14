06f024db44d853716523263c5a01e8c63bdadaeb
unknown <gdutt2304@gmail.com>
2026-01-14 15:46:20 +0530

    task

---
+++ express/htmlConnect/app1.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+const users = [
+  {
+    id: 1,
+    name: "Aditya",
+    age: 21,
+    details:
+      "    Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, ipsam!",
+  },
+  {
+    id: 2,
+    name: "Rajneesh",
+    age: 23,
+    details:
+      "    Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, ipsam!",
+  },
+  {
+    id: 3,
+    name: "Amar",
+    age: 25,
+    details:
+      "    Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, ipsam!",
+  },
+  {
+    id: 4,
+    name: "Aditi",
+    age: 16,
+    details:
+      "    Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, ipsam!",
+  },
+  {
+    id: 5,
+    name: "Gourav",
+    age: 21,
+    details:
+      "    Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, ipsam!",
+  },
+];
+
+app.get("/api/users", (req, res) => {
+  res.json(users);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port PORT`);
+});
