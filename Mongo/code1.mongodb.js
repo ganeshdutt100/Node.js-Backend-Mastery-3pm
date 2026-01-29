1964937349f9757c35fb1a79d145035f06d37afb
unknown <gdutt2304@gmail.com>
2026-01-29 14:15:52 +0530

    Task Mongo

--- Mongo/code1.mongodb.js
+++ Mongo/code1.mongodb.js
@@ -37,3 +37,9 @@ use("BackendClass");
 // db.code4.find({ name: "a" });
 // db.code4.deleteOne({ name: "a" });
 db.code4.find();
+
+// collection->// products->
+// 5product insert (name,price,category,stock)->
+// 500 < find
+// Electronic  update  - 500 -> 100
+// stock -  0 Delete
