ef2637c2a0be4d7bed7f4f4ff7e8bbd1e90f40c0
unknown <gdutt2304@gmail.com>
2026-01-29 14:11:07 +0530

    Update Mongo

---
+++ Mongo/code1.mongodb.js
@@ -0,0 +1,39 @@
+use("BackendClass");
+// db.code4.insertMany([
+//   {
+//     name: "a",
+//     email: "a@gmial.com",
+//     age: 23,
+//   },
+//   {
+//     name: "b",
+//     email: "b@gmial.com",
+//     age: 13,
+//   },
+//   {
+//     name: "c",
+//     email: "c@gmial.com",
+//     age: 20,
+//   },
+//   {
+//     name: "d",
+//     email: "d@gmial.com",
+//     age: 23,
+//   },
+//   {
+//     name: "e",
+//     email: "e@gmial.com",
+//     age: 25,
+//   },
+// ]);
+// db.code4.find({ age: { $lt: 23 } });
+// db.code4.find({}, { name: 1, email: 1, _id: 0, age: 1 });
+// db.code4.find({ $or: [{ name: "a" }, { age: 23 }] });
+
+// 0; // Hide
+// 1; // show
+
+// db.code4.updateOne({ name: "a" }, { $set: { phone: "3847827382837" } });
+// db.code4.find({ name: "a" });
+// db.code4.deleteOne({ name: "a" });
+db.code4.find();
