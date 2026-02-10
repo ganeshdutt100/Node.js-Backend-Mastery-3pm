57d9977c03b6b5e607837e563f449908549aaded
unknown <gdutt2304@gmail.com>
2026-02-10 14:20:23 +0530

    FeedbackForm

---
+++ EJS/backend/models/Feedback.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const FeedbackSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  message: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model("Feedback", FeedbackSchema);
--- EJS/backend/server.js
+++ EJS/backend/server.js
@@ -1,14 +1,40 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const Feedback = require("./models/Feedback");
 const app = express();
 
 app.use(express.json());
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
+// mongoose
+mongoose
+  .connect("mongodb://localhost:27017/feedback_db")
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  const userName = "Ganesh Dutt";
-  res.render("index", { name: userName });
+  res.render("feedback");
+});
+
+app.post("/submit-feedback", async (req, res) => {
+  try {
+    const newFeedback = new Feedback({
+      name: req.body.name,
+      email: req.body.email,
+      message: req.body.message,
+    });
+    await newFeedback.save();
+    res.render("success", { name: req.body.name });
+  } catch (err) {
+    console.log(err);
+    res.send("Error saving feedback");
+  }
 });
 
 app.listen(PORT, () => {
