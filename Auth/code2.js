5aa116226045304ca40a6d43fdbc3d1ddb7f29d1
unknown <gdutt2304@gmail.com>
2026-02-17 15:35:43 +0530

    bcrypt

--- Auth/code2.js
+++ Auth/code2.js
@@ -17,7 +17,7 @@ app.get("/", (req, res) => {
 // };
 // hashPassword();
 
-// const password = "Ganesh@123";
+const password = "Ganesh@123";
 // bcrypt.genSalt(10, function (err, salt) {
 //   bcrypt.hash(password, salt, function (err, hash) {
 //     if (err) return console.error(err);
@@ -27,6 +27,14 @@ app.get("/", (req, res) => {
 //     console.log("hashed  password  +  salt :  ", hash);
 //   });
 // });
+const verifyPassword = async () => {
+  const password = "Ganesh@123";
+  const hashPassword =
+    "$2b$10$N7qZ8bJWtd1RvvpjV4pF0.BOhilcw1SPtneK7.e0p/U.fiN3ULEOK";
+  const match = await bcrypt.compare(password, hashPassword);
+  console.log(match ? "password Correct" : "Password Wrong");
+};
+verifyPassword();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
